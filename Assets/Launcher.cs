0cd9433bdbf97cdbf933ca8b52d5843fc518270f
Tsnat Kelemework <68676538+buzzeres@users.noreply.github.com>
2023-10-06 15:09:13 -0400

    last push

--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public float launchSpeed = 10;
     public GameObject projectileToCopy;
     public float launchElevation = 30;
@@ -17,10 +19,35 @@ public class Launcher : MonoBehaviour
     }
 
     // Update is called once per frame
+=======
+    public float launchSpeed = 10.0f;
+    public GameObject projectileToCopy;
+    public float launchElevation = 30;
+
+    // Update is called once per frame
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newProjectile = Instantiate(projectileToCopy);
+            Tsnats_Body tsnats = newProjectile.GetComponent<Tsnats_Body>();
+            newProjectile.transform.position = transform.position;
+
+            tsnats.velocity = new Vector3(
+                Mathf.Cos(launchElevation * Mathf.Deg2Rad * launchSpeed),
+                    Mathf.Sin(launchElevation * Mathf.Deg2Rad * launchSpeed), 
+                0);
+        }
+
+    }
+    
+>>>>>>> Stashed changes
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+<<<<<<< Updated upstream
             GameObject newProjectile = Instantiate(projectileToCopy);
             newProjectile.transform.position = transform.position;
 
@@ -34,6 +61,9 @@ public class Launcher : MonoBehaviour
             tsnats.velocity = new Vector3(launchElevation * Mathf.Deg2Rad* launchSpeed,
                                             launchElevation* Mathf.Deg2Rad*launchSpeed,
                 0);
+=======
+            Shoot();
+>>>>>>> Stashed changes
         }
-}
+    }
 }
--- Assets/Tsnats_World.cs
+++ Assets/Tsnats_World.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Tsnats_World : MonoBehaviour
+public class TsnatsWorld : MonoBehaviour
 {
     public float dt = 1.0f/30.0f;    // Start is called before the first frame update
     public float t = 0.0f;
-    public List<Tsnats_World> bodies;
+    public List<Tsnats_Body> bodies;
     public Vector3 gravity = new Vector3(0, -9.8f, 0);
 
     void Start()
     {
-        bodies = new List<Tsnats_World>();
+        bodies = new List<Tsnats_Body>();
         dt = Time.fixedDeltaTime;
 
     }
@@ -21,7 +21,13 @@ public class Tsnats_World : MonoBehaviour
     {
         Tsnats_Body[] foundBodies = FindObjectOfType<Tsnats_Body>();
 
+<<<<<<< Updated upstream
         foreach (Tsnats_Body foundBody in foundBodies)
+=======
+        Tsnats_Body[] allBodies = FindObjectsOfType< Tsnats_Body > (false);
+
+        foreach (Tsnats_Body foundBody in allBodies)
+>>>>>>> Stashed changes
         {
             
         }
@@ -33,16 +39,21 @@ public class Tsnats_World : MonoBehaviour
         AddNewodiesFromScene();
         foreach (Tsnats_Body body in bodies)
         {
+<<<<<<< Updated upstream
             body.velocity += gravity * body.gravityScale * dt;
             body.transform.position += body.velocity + dt;
+=======
+            {
+                body.velocity += gravity * body.gravityScale * dt;
+
+                body.transform.position += body.velocity * dt;
+
+            }
+>>>>>>> Stashed changes
         }
+       
 
         t += dt;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
