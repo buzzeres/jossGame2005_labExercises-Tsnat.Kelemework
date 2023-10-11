d2e63c6416f1040988a8ea4cbfd49dd02f3eb3cb
Tsnat Kelemework <68676538+buzzeres@users.noreply.github.com>
2023-10-10 20:09:34 -0400

    Getting files ready and setting up the thysucs Body

--- Assets/Tsnats_Body.cs
+++ Assets/Tsnats_Body.cs
@@ -8,4 +8,6 @@ public class Tsnats_Body : MonoBehaviour
     public Vector3 velocity = Vector3.zero;
     public float gravityScale = 1.0f;
     public float AirFriction = 0.1f;
+    public ThysicsBody ThysicsBody;  // Reference to the physics body
+
 }
